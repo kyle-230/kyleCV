96031b90ad9dbc8dd57c843ac0ffccb089ddd819
kyle-230 <57258533+kyle-230@users.noreply.github.com>
2020-05-07 14:53:00 +0400

    back to normal

--- js/script.js
+++ js/script.js
@@ -12,33 +12,7 @@ $(function () {
     );
   });
 
-  $(".btn-cv").on("click", function () {
+  $(".button1").on("click", function () {
     alert("inbox me on social media to get CV");
-  });
-  $("#contact-form").submit(function (e) {
-    e.preventDefault();
-    $(".comments").empty();
-    var postdata = $("#contact-form").serialize();
-
-    $.ajax({
-      type: "POST",
-      url: "php/contact.php",
-      data: postdata,
-      dataType: "json",
-      success: function (json) {
-        if (json.isSuccess) {
-          $("#contact-form").append(
-            "<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>"
-          );
-          $("#contact-form")[0].reset();
-        } else {
-          $("#firstname + .comments").html(json.firstnameError);
-          $("#name + .comments").html(json.nameError);
-          $("#email + .comments").html(json.emailError);
-          $("#phone + .comments").html(json.phoneError);
-          $("#message + .comments").html(json.messageError);
-        }
-      },
-    });
   });
 });
